ac840cf5a3f7332f0d0d1969fd6ea56259d01d24
Ivan Petrov <vipsister2014@gmail.com>
2020-11-01 13:01:38 +0200

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,46 +21,52 @@ module.exports = {
 
 	post: {
 		register(req, res, next) {
-			const { email, fullName, password } = { ...req.body };
-
-			User.findOne({ email })
-				.then((user) => {
-					if (user) {
-						throw new Error('The given email is already in use...');
-					}
-					return User.create({ email, fullName, password });
-				})
-				.then((createdUser) => {
-					res.redirect('/user/login');
-				})
-				.catch((e) => {
-					console.log(e);
-					res.redirect('/user/register');
-				});
+			console.log(req.body);
 		},
-
 		login(req, res, next) {
-			const { email, password } = req.body;
-
-			User.findOne({ email })
-				.then((user) => {
-					return Promise.all([user.comparePasswords(password), user]);
-				})
-				.then(([isPasswordsMatched, user]) => {
-					if (!isPasswordsMatched) {
-						throw new Error('The provided password does not matched.');
-					}
-
-					const token = jwt.createToken(user._id);
-
-					res
-						.status(200)
-						.cookie(cookie, token, { maxAge: 3600000 })
-						.redirect('/shoes/all');
-				})
-				.catch((e) => {
-					console.log(e);
-				});
+			console.log(req.body);
 		},
 	},
+	// 	register(req, res, next) {
+	// 		const { email, fullName, password } = { ...req.body };
+
+	// 		User.findOne({ email })
+	// 			.then((user) => {
+	// 				if (user) {
+	// 					throw new Error('The given email is already in use...');
+	// 				}
+	// 				return User.create({ email, fullName, password });
+	// 			})
+	// 			.then((createdUser) => {
+	// 				res.redirect('/user/login');
+	// 			})
+	// 			.catch((e) => {
+	// 				console.log(e);
+	// 				res.redirect('/user/register');
+	// 			});
+	// 	},
+
+	// 	login(req, res, next) {
+	// 		const { email, password } = req.body;
+
+	// 		User.findOne({ email })
+	// 			.then((user) => {
+	// 				return Promise.all([user.comparePasswords(password), user]);
+	// 			})
+	// 			.then(([isPasswordsMatched, user]) => {
+	// 				if (!isPasswordsMatched) {
+	// 					throw new Error('The provided password does not matched.');
+	// 				}
+
+	// 				const token = jwt.createToken(user._id);
+
+	// 				res
+	// 					.status(200)
+	// 					.cookie(cookie, token, { maxAge: 3600000 })
+	// 					.redirect('/shoes/all');
+	// 			})
+	// 			.catch((e) => {
+	// 				console.log(e);
+	// 			});
+	// 	},
 };
--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const getUserModel = require('./User');
-// const getShoeModel = require('./Shoe');
+const getShoeModel = require('./Shoe');
 
 module.exports = {
 	User: getUserModel(mongoose, bcrypt),
